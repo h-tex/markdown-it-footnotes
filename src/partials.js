397d9fb78f1de6361d1fd023cd60552350eac496
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-09-12 04:44:15 +0200

    Use the document ID as a prefix to make the footnote anchor unique (#3)

--- src/partials.js
+++ src/partials.js
@@ -6,8 +6,8 @@ export function render_footnote_anchor_name (tokens, idx, options, env/* , slf *
 	const n = Number(tokens[idx].meta.id + 1).toString();
 	let prefix = "";
 
-	if (typeof env.docId === "string") {
-		prefix = `-${env.docId}-`;
+	if (typeof env.id === "string") {
+		prefix = `${env.id}-`; // use the document ID as a prefix to make the footnote anchor unique
 	}
 
 	return prefix + n;
@@ -44,6 +44,7 @@ export function render_footnote_ref (tokens, idx, options, env, slf) {
 
 export function render_footnote_open (tokens, idx, options, env, slf) {
 	let id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
+	let caption = slf.rules.footnote_caption(tokens, idx, options, env, slf);
 
 	if (tokens[idx].meta.subId > 0) {
 		id += `:${tokens[idx].meta.subId}`;
@@ -55,7 +56,7 @@ export function render_footnote_open (tokens, idx, options, env, slf) {
 		attrs += ` epub:type="footnote"`;
 	}
 
-	return `<article ${ attrs }><a href="#fn-${ id }" class="footnote-marker">${ id }</a>`;
+	return `<article ${ attrs }><a href="#fn-${ id }" class="footnote-marker">${ caption }</a>`;
 }
 
 export function render_footnote_close () {
